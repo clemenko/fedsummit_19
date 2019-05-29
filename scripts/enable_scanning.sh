ff490d5962c64b7c6dfd427745daa1eac269fce4
gosunilgo <gosunilgo@users.noreply.github.com>
2019-05-29 11:16:02 -0400

    fix directory of fedsummit
    fix directory of fedsummit

--- scripts/enable_scanning.sh
+++ scripts/enable_scanning.sh
@@ -3,7 +3,7 @@
 
 if [ -z ${UCP_URL+x} ]; then
   echo Setting variables for you...
-  . ~/fedsummit_2019/scripts/var_setup.sh
+  . ~/fedsummit_19/scripts/var_setup.sh
 fi
 
 curl -kX POST --user $DTR_USERNAME:$DTR_ADMIN_PASSWORD "https://$DTR_URL/api/v0/meta/settings" -H "Content-Type: application/json" -H "Accept: application/json"  -d '{ "reportAnalytics": false, "anonymizeAnalytics": false, "disableBackupWarning": true, "scanningEnabled": true, "scanningSyncOnline": true, "scanningEnableAutoRecheck": true }' > /dev/null 2>&1
